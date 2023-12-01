9cfb0f2a0ebbae319c5fa314d9544cf4d12fe387
tatsuyakawase <136999981+tatsuyakawase@users.noreply.github.com>
2023-12-01 23:03:02 +0900

    change migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name',100);
-            $table->string('email')->unique();
+            $table->string('email')->charset("utf8")->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->tinyinteger('role')->default(0);
